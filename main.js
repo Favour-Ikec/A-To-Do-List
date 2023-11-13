3397e73e8a24e5de798c3573598da186ef7e3ba1
Favour Ikechukwu <149949474+Favour-Ikec@users.noreply.github.com>
2023-11-13 22:55:25 +0100

    Update and rename script.js to main.js

--- main.js
+++ main.js
@@ -3,18 +3,14 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 
-
-
 const currentYear = new Date().getFullYear();
 
 const newYearDate = new Date(`January 1 ${currentYear + 1}, 00:00:00`);
-
 
 function updateCountDown() {
   const currentTime = new Date();
 
   const diff = newYearDate - currentTime;
-
   const d = Math.floor(diff / 1000 / 60 / 60 / 24 );
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
   const m = Math.floor(diff / 1000 / 60) % 60;
@@ -22,15 +18,8 @@ function updateCountDown() {
   
 
   let days = document.getElementById("days").innerHTML = d;
-  
-  
   let hours = document.getElementById("hours").innerHTML = h < 10 ? "0" + h : h;
-  
-  
   let minutes = document.getElementById("minutes").innerHTML = m < 10 ? "0" + m : m; 
-  
-  
-  
   let seconds = document.getElementById("seconds").innerHTML = s < 10 ? "0" + s : s;
 
   if(diff <= 0) {
@@ -40,7 +29,6 @@ function updateCountDown() {
 }
 
 setInterval(updateCountDown, 1000);
-
 
   const celebrationMessages = [
     "Happy New Year!",
@@ -55,28 +43,13 @@ setInterval(updateCountDown, 1000);
     "May the next year be a chapter of endless blessings.",
     "Happy New Year! May it be your best one yet.",
     "May the coming year bring you peace, health, and happiness."
-
-
-
-
-
   ]
 
   function celebrateNewYear () {
-      //First we need a variable to display random messages
       const celebrationMessage = document.getElementById("celebration-message");
 
-      // Randomly select a message
       const randomMessage =
         celebrationMessages[Math.floor(Math.random() * celebrationMessages.length)];
-    
-      // Set the message text
-      celebrationMessage.textContent = randomMessage;
-    
-      // Show the message by removing the "hidden" class
-      celebrationMessage.classList.remove("hidden");
+        celebrationMessage.textContent = randomMessage;
+        celebrationMessage.classList.remove("hidden");
 }
-
-
-
-
